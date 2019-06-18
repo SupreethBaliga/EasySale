e9baa0ed15b8f1e00e00a0e4ca1b2fc6430ddb2a
SupreethBaliga <supreethbaliga@gmail.com>
2019-06-18 18:08:06 +0530

    Changes made to default in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 //import ProductPage from './ProductPage/ProductPage';
 //import ProductListPage from './ProductListPage/ProductListPage';
-import OrderList from './OrderList/OrderList';
+//import OrderList from './OrderList/OrderList';
 class App extends Component {
   // constructor(props) {
   //   super(props);
@@ -13,70 +13,12 @@ class App extends Component {
   //   }
   // }
 
-  props = {
-    orders: [
-      {
-        orderNumber: 1,
-        status: 'Pending',
-        expectedDate: '12/6/19'
-      },
-      {
-        orderNumber: 2,
-        status: 'Processed',
-        expectedDate: '10/6/19'
-      },
-      {
-        orderNumber: 3,
-        status: 'Checking',
-        expectedDate: '8/6/19'
-      },
-      {
-        orderNumber: 4,
-        status: 'Dispatched',
-        expectedDate: '7/6/19'
-      },
-      {
-        orderNumber: 5,
-        status: 'Delivered',
-        expectedDate: '5/6/19'
-      }
-    ]
-  }
-
   render() {
 
-    let props = {
-      orders: [
-        {
-          orderNumber: 1,
-          status: 'Pending',
-          expectedDate: '12/6/19'
-        },
-        {
-          orderNumber: 2,
-          status: 'Processed',
-          expectedDate: '10/6/19'
-        },
-        {
-          orderNumber: 3,
-          status: 'Checking',
-          expectedDate: '8/6/19'
-        },
-        {
-          orderNumber: 4,
-          status: 'Dispatched',
-          expectedDate: '7/6/19'
-        },
-        {
-          orderNumber: 5,
-          status: 'Delivered',
-          expectedDate: '5/6/19'
-        }
-      ]
-    }
+    
     return (
       <div>
-        <OrderList {...props} />
+        Hello
       </div>
     )
   }
