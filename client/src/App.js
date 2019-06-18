a07cfa081a1f4d6fd82a22c3b64ab4cad5338f26
SupreethBaliga <supreethbaliga@gmail.com>
2019-06-18 17:40:26 +0530

    OrderList and OrderListItem components have been added.

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,80 @@ import React, { Component } from 'react';
 import './App.css';
 //import ProductPage from './ProductPage/ProductPage';
 //import ProductListPage from './ProductListPage/ProductListPage';
+import OrderList from './OrderList/OrderList';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultValue: "100",
-      step: 100
-    }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     defaultValue: "100",
+  //     step: 100
+  //   }
+  // }
+
+  props = {
+    orders: [
+      {
+        orderNumber: 1,
+        status: 'Pending',
+        expectedDate: '12/6/19'
+      },
+      {
+        orderNumber: 2,
+        status: 'Processed',
+        expectedDate: '10/6/19'
+      },
+      {
+        orderNumber: 3,
+        status: 'Checking',
+        expectedDate: '8/6/19'
+      },
+      {
+        orderNumber: 4,
+        status: 'Dispatched',
+        expectedDate: '7/6/19'
+      },
+      {
+        orderNumber: 5,
+        status: 'Delivered',
+        expectedDate: '5/6/19'
+      }
+    ]
   }
 
   render() {
+
+    let props = {
+      orders: [
+        {
+          orderNumber: 1,
+          status: 'Pending',
+          expectedDate: '12/6/19'
+        },
+        {
+          orderNumber: 2,
+          status: 'Processed',
+          expectedDate: '10/6/19'
+        },
+        {
+          orderNumber: 3,
+          status: 'Checking',
+          expectedDate: '8/6/19'
+        },
+        {
+          orderNumber: 4,
+          status: 'Dispatched',
+          expectedDate: '7/6/19'
+        },
+        {
+          orderNumber: 5,
+          status: 'Delivered',
+          expectedDate: '5/6/19'
+        }
+      ]
+    }
     return (
       <div>
-        Hello
+        <OrderList {...props} />
       </div>
     )
   }
