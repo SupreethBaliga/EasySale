3372ace618b2c7656a3dcb2d7e31bc183f2075f3
SupreethBaliga <supreethbaliga@gmail.com>
2019-07-03 19:10:01 +0530

    AddProduct has now been linked to backend. Functionality added to upload images and display.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
+const multer = require('multer');
 import 'babel-polyfill';
 import Product from './src/controllers/Product';
 import User from './src/controllers/User';
-import Order from './src/controllers/Order'
+import Order from './src/controllers/Order';
 var cors = require('cors');
 
 const port = 8000;
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'Testing for Server'});
+	return res.status(200).send({ 'message': 'Testing for Server' });
 })
 
 app.post('/api/products/', Product.create);
@@ -22,6 +23,7 @@ app.put('/api/products/:id', Product.update);
 app.delete('/api/products/:id', Product.delete);
 
 app.post('/api/users/', User.create);
+app.post('/api/users/login/', User.login);
 app.get('/api/users/', User.getAll);
 app.get('/api/users/:id', User.getOne);
 app.put('/api/users/:id', User.update);
@@ -31,6 +33,26 @@ app.get('/api/orders/', Order.getAll);
 app.get('/api/orders/:orderNumber', Order.getOne);
 app.put('/api/orders/:orderNumber', Order.update);
 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '../seller/src/assets/images');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/api/imageupload', upload.single('imagefile'), function (req, res) {
+	if (!req.file) return res.status(400);
+	else return res.status(200);
+});
+
+app.use(function (err, req, res) {
+	if (err instanceof multer.MulterError) res.status(500).send(err.message);
+});
+
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+	console.log(`App running on port ${port}`);
 })
